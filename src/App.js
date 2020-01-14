e451cbbc333e486132a11cace0f98262361fb6f5
oluwadareseyi <oluwadareseyii@gmail.com>
2020-01-14 12:45:14 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.scss';
+
+const app = () => {
+  return (
+    <div className="app">
+        <h1>Hello World!</h1>
+    </div>
+  );
+}
+
+export default app;
